81dec7b0731ae41175ae6c1e95d7a3c7df33dc83
Conrado Alexsander Soares Gonçalves <53683786+conradoalexsander@users.noreply.github.com>
2020-04-27 20:05:11 -0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.unsplash.com/",
     headers: {
-        Authorization: "Client-ID wW2Z9Sbkg0LDedWPTN_IEZHN_LEOHQyDPC03f0uL0fo"
+        Authorization: "Client-ID YOUR API KEY HERE"
       }
 })
